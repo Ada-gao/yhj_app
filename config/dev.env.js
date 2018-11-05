581ecac883cc4c3f20f187524fe81167cb902547
baoyan <baoyan@shuyun365.com>
2018-11-05 17:48:28 +0800

    updata

--- config/dev.env.js
+++ config/dev.env.js
@@ -13,8 +13,8 @@ module.exports = merge(prodEnv, {
 })
 
 // module.exports = merge(prodEnv, {
-//   NODE_ENV: '"development"',
-//   BASE_API: '"http://10.9.70.61:8080/api"'
-// })
+// //   NODE_ENV: '"development"',
+// //   BASE_API: '"http://10.9.70.61:8080/api"'
+// // })
 
 
